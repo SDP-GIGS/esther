7ca24a6d6015f2267de1295874cbf4a33b7ce92b
angellamurungi91-snow <angellamurungi91@gmail.com>
2026-05-04 12:51:40 -0700

    made changes

--- backend/backend/backendapp/models.py
+++ backend/backend/backendapp/models.py
@@ -45,6 +45,9 @@ class Report(models.Model):
     status = models.CharField(max_length=20, choices=STATUS, default='draft')
     submission_date = models.DateField(auto_now_add=True)
 
+    def __str__(self):
+        return f"{self.student}-({self.status})"
+
 
 
 
