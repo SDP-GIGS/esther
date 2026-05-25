83951b6e30d053fa3725f57fdf0ace4eb8ae55a5
vannybianca <kisembovanessa6@gmail.com>
2026-05-25 17:12:50 +0300

    Update AdminDashboard.jsx with my changes

--- frontend/frontend-app/src/Dashboards/AdminDashboard.jsx
+++ frontend/frontend-app/src/Dashboards/AdminDashboard.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { useUser } from "../UserContext";
+import { useUser } from "../context/UserContext";
 
 export default function AdminDashboard() {
   const { user } = useUser();
