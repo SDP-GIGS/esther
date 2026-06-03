df3c6b1797937d0692e37e723f049d558b580658
chistinether <christinestheri@gmail.com>
2026-06-03 11:41:51 +0300

    modified settings.py

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -28,7 +28,19 @@ SECRET_KEY = 'django-insecure-garo(!gl6&(!ak!pmg7irncg&kua-)6yg_ka%%%+4jw^6=7rmu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+CORS_ALLOWED_ORIGINS = [
+    "https://esther-api.tagooledavid.com",
+    "http://localhost:5173",
+    "http://127.0.0.:5173",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://esther-api.tagooledavid.com",
+    "http://127.0.0.1:5173",
+    "http://localhost:5173",
+]
+]
 
 
 # Application definition
@@ -47,6 +59,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -54,10 +67,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
 ROOT_URLCONF = 'backend.urls'
 AUTH_USER_MODEL = 'backendapp.User'
 
